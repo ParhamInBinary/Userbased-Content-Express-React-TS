687260e7fd9fa0259e3e3becb77137a32f6ba3de
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-04-14 11:41:36 +0200

    add routes
    Co-authored-by: Parham Berenjian <ParhamInBinary@users.noreply.github.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,20 @@
-  return (
- <></>
-  )
-}
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./Home";
+import { Login } from "./Login";
+import { Navbar } from './Navbar';
+import { Register } from "./Register";
 
-export default App
+export default function App() {
+  return(
+    <>
+    <Navbar />
+    <main>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+    </main>
+    </>
+  )
+};
