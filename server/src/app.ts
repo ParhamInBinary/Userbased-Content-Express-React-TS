4def934da1569de08b2663ffb9642b0079b67f18
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-04-18 15:15:07 +0200

    server routes setup
    Co-authored-by: Parham Berenjian <ParhamInBinary@users.noreply.github.com>

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,10 @@
 import express from 'express';
+import 'express-async-errors';
+import { postRouter } from './resources/posts/post-router';
+import { userRouter } from './resources/users/user-router';
 
 export const app = express();
-
 // SKRIV DIN SERVERKOD HÄR!
+app.use(express.json());
+app.use(userRouter);
+app.use(postRouter);
