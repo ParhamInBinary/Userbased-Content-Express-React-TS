d4f3bfb1c2c28f9e093375262c5a2f5f5fa3bc41
ParhamInBinart <parham.berenjian@gmail.com>
2023-04-19 13:36:13 +0200

    added cookie-session

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,20 @@
-import express from 'express';
-import 'express-async-errors';
-import { postRouter } from './resources/posts/post-router';
-import { userRouter } from './resources/users/user-router';
+import cookieSession from "cookie-session";
+import express from "express";
+import "express-async-errors";
+import { postRouter } from "./resources/posts/post-router";
+import { userRouter } from "./resources/users/user-router";
 
 export const app = express();
 // SKRIV DIN SERVERKOD HÄR!
 app.use(express.json());
+app.use(
+    cookieSession({
+        name: 'login',
+        secure: false,
+        httpOnly: true,
+        secret: 'sdg669s8dguiksg7993kjbh1',
+        maxAge: 1000 * 20,
+    })
+);
 app.use(userRouter);
 app.use(postRouter);
