b22c1524bf2406931b1de39fb5c047bdd6fa5ce7
ParhamInBinart <parham.berenjian@gmail.com>
2023-04-20 11:56:11 +0200

    template for types

--- server/src/resources/posts/post-model.ts
+++ server/src/resources/posts/post-model.ts
@@ -2,13 +2,15 @@ import { InferSchemaType, Schema, model } from "mongoose";
 
 export const postSchema = new Schema({
   title: { type: String, required: true },
-  content: { type: String, required: true, maxlenght: 400 },
-  author: { type: String, required: false },
+  content: { type: String, required: true, maxlength: 400 },
+  author: { type: String, required: true },
 }, {
   versionKey: false,
   timestamps: true,
 });
 
-export type Post = InferSchemaType<typeof postSchema>;
+export type Post = InferSchemaType<typeof postSchema> & { _id: string };
+// export type PostCreate = Omit<Post, '_id' | 'createdAt' | 'updatedAt' | 'author'>;
+// export type PostCreateAlt = Pick<Post, 'title' | 'content'>;
 
 export const PostModel = model("post", postSchema);
