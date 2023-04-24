6a5f4fb99ce73004dbc8a7a7d62579a209d88fcf
ParhamInBinart <parham.berenjian@gmail.com>
2023-04-25 00:08:10 +0200

    added logic for logging in

--- client/src/Navbar/Login.tsx
+++ client/src/Navbar/Login.tsx
@@ -1,69 +1,99 @@
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { Box, Button, TextField } from "@mui/material";
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function Login() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     handle login logic here
-//   };
+  const handleLogin = async (e: any) => {
+    e.preventDefault();
+
+    const newUser = {
+      username,
+      password,
+    };
+
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify(newUser),
+      headers: { "Content-type": "application/json" },
+    });
+
+    const data = await response.json()
+
+    if (response.ok) {
+      localStorage.setItem('loggedInUsername', data.username)
+      localStorage.setItem('loggedInUserID', data._id)
+      localStorage.setItem('loggedInIsAdmin', data.isAdmin)
+      
+      navigate('/');
+    }
+  };
 
   return (
     <>
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        padding: "0 16px",
-        flexDirection: 'column'
-      }}
-    >
-        <AccountCircleIcon sx={{ fontSize: 96, color: 'lightgray' }} />
-      <Box sx={{ width: "100%", maxWidth: "400px" }}>
-        <form >
-          <TextField
-            id="username"
-            label="Username"
-            type="username"
-            fullWidth
-            required
-            margin="normal"
-            variant="outlined"
-          />
-          <TextField
-            id="password"
-            label="Password"
-            type="password"
-            fullWidth
-            required
-            margin="normal"
-            variant="outlined"
-          />
-          <Button
-            type="submit"
-            variant="contained"
-            size="large"
-            sx={{ mt: 3 }}
-            fullWidth
-          >
-            Sign In
-          </Button>
-          <Button
-            variant="outlined"
-            size="large"
-            sx={{ mt: 3 }}
-            fullWidth
-            onClick={() => navigate('/register')}
-          >
-            Register
-          </Button>
-        </form>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          padding: "0 16px",
+          flexDirection: "column",
+        }}
+      >
+        <AccountCircleIcon
+          sx={{ fontSize: 96, color: "lightgray" }}
+        />
+        <Box sx={{ width: "100%", maxWidth: "400px" }}>
+          <form>
+            <TextField
+              id="username"
+              label="Username"
+              type="username"
+              fullWidth
+              required
+              margin="normal"
+              variant="outlined"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <TextField
+              id="password"
+              label="Password"
+              type="password"
+              fullWidth
+              required
+              margin="normal"
+              variant="outlined"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              size="large"
+              sx={{ mt: 3 }}
+              fullWidth
+              onClick={handleLogin}
+            >
+              Sign In
+            </Button>
+            <Button
+              variant="outlined"
+              size="large"
+              sx={{ mt: 3 }}
+              fullWidth
+              onClick={() => navigate("/register")}
+            >
+              Register
+            </Button>
+          </form>
+        </Box>
       </Box>
-    </Box>
     </>
   );
 }
