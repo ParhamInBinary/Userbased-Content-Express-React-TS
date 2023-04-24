514a1226765ade09198a87a8f59fbfcd55772b57
ParhamInBinart <parham.berenjian@gmail.com>
2023-04-25 00:08:28 +0200

    added logic for login/logout button

--- client/src/Navbar/Navbar.tsx
+++ client/src/Navbar/Navbar.tsx
@@ -1,21 +1,54 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { useNavigate } from 'react-router-dom';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { useNavigate } from "react-router-dom";
+import { checkIsLoggedIn } from "../checkIsLoggedIn";
 
 export function Navbar() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const isLoggedIn = checkIsLoggedIn();
+
+  const handleLogout = async (e: any) => {
+    e.preventDefault();
+
+    const response = await fetch("/api/users/logout", {
+      method: "POST",
+    });
+
+    if (response.ok) {
+      localStorage.clear();
+      navigate("/");
+    }
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="fixed">
         <Toolbar>
-          <Typography style={{cursor: 'pointer'}} variant="h6" component="div" sx={{ flexGrow: 1 }} onClick={() => navigate('/')}>
+          <Typography
+            style={{ cursor: "pointer" }}
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}
+            onClick={() => navigate("/")}
+          >
             Home
           </Typography>
-          <Button color="inherit" onClick={() => navigate('/login')}>Login</Button>
+          {isLoggedIn ? (
+            <Button color="inherit" onClick={handleLogout}>
+              Logout
+            </Button>
+          ) : (
+            <Button
+              color="inherit"
+              onClick={() => navigate("/login")}
+            >
+              Login
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
