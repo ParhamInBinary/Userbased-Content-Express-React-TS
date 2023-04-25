732ee3b267caa36cc39d12271fb82f54ffebb7a4
ParhamInBinart <parham.berenjian@gmail.com>
2023-04-25 18:04:48 +0200

    createAt attribute added to postschema

--- server/src/resources/posts/post-model.ts
+++ server/src/resources/posts/post-model.ts
@@ -1,16 +1,22 @@
 import { InferSchemaType, Schema, model } from "mongoose";
 
-export const postSchema = new Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  author: { type: String, required: true },
-}, {
-  versionKey: false,
-  timestamps: true,
-});
+export const postSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: { type: String, required: true },
+  },
+  {
+    versionKey: false,
+    timestamps: { createdAt: true, updatedAt: false },
+  }
+);
 
-export type Post = InferSchemaType<typeof postSchema> & { _id: string };
+export type Post = InferSchemaType<typeof postSchema> & {
+  _id: string;
+  createdAt: string;
+};
 // export type PostCreate = Omit<Post, '_id' | 'createdAt' | 'updatedAt' | 'author'>;
 // export type PostCreateAlt = Pick<Post, 'title' | 'content'>;
 
-export const PostModel = model("post", postSchema);
+export const PostModel = model<Post>("post", postSchema);
