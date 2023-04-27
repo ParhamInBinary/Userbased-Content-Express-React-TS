83ef2c19f4a1208426cde5185655930b0821c2e9
ParhamInBinart <parham.berenjian@gmail.com>
2023-04-27 17:37:16 +0200

    minor styling changes

---
+++ client/src/Admin/AdminPage.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import UserCard from "./UserCard";
+
+export interface User {
+  _id: number;
+  username: string;
+  isAdmin: boolean;
+}
+
+export function AdminPage() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  const getUsers = async () => {
+    const response = await fetch("/api/users");
+    const data = await response.json();
+
+    if (response.ok) {
+      setUsers(data);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: "6rem",
+        }}
+      >
+        {users.map((user) => (
+          <UserCard
+            key={user._id}
+            user={{
+              _id: user._id,
+              Username: user.username,
+              isAdmin: user.isAdmin,
+            }}
+            getUsers={getUsers}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
