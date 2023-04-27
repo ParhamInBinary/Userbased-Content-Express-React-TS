8ddf7b1b5c99c153352df41cd1dddbe485cf13ff
ParhamInBinart <parham.berenjian@gmail.com>
2023-04-27 16:49:36 +0200

    moved file + prettier

---
+++ client/src/Navbar/AdminButton.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+export const AdminButton: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleNavigateToAdmin = () => {
+    navigate("/admin");
+  };
+
+  return (
+    <Button
+      style={{
+        borderRight: " 1px solid lightgrey",
+        borderRadius: "0",
+      }}
+      color="inherit"
+      onClick={handleNavigateToAdmin}
+    >
+      View Users
+    </Button>
+  );
+};
